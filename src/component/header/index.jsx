fa1795a879680463eba9e8a4dbe9f915e477294a
sulsul2 <110590472+sulsul2@users.noreply.github.com>
2023-05-28 14:27:31 +0700

    header + nav

--- src/component/header/index.jsx
+++ src/component/header/index.jsx
@@ -1,8 +1,22 @@
 import Main from '../../assets/mainlogo.svg'
+import './style.css'
 const Header = () => {
     return (
         <>
-            <img src = {Main}/>
+            <div className="head">
+                <a href = "/">
+                    <img src={Main}/>
+                </a>
+                <div className="nav">
+                    <ul>
+                        <li><a href = "/">Dashboard</a></li>
+                        <li><a href = "/">HMI</a></li>
+                        <li><a href = "/">Database</a></li>
+                        <li><a href = "/">Profile</a></li>
+                        <li><a href = "/">Logout</a></li>
+                    </ul>
+                </div>
+            </div>  
         </>
     )
 }
