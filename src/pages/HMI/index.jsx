3fb4e31b4584dc2312f497eb948adf0f57485d63
Muhammad Hanif Al Faithoni <et4w4zz@gmail.com>
2023-06-01 09:39:18 +0700

    kurang responsif

--- src/pages/HMI/index.jsx
+++ src/pages/HMI/index.jsx
@@ -1,14 +1,23 @@
 import Header from '../../component/header'
 import './style.css'
-import RCT from '../../assets/Gambar Ilustrasi Mesin.png';
+import IM from '../../assets/Gambar Ilustrasi Mesin (No BG).png';
 
 const HMI = () => {
     return (
         <>
         <Header />
-        <div className='gambar'>
-            <img src= {RCT} alt="" />
+        <div className='ilustrasi'>
+            <img src= {IM} alt="Gambar Ilustrasi Mesin" />
+            <div className='T1'>
+                <label htmlFor="a">T1</label>
+                <input type="number" id='a' name='a' placeholder='T1'/>
+            </div>
+            <div className='T3'>
+                <label htmlFor="b">T3</label>
+                <input type="number" id='b' name='b' placeholder='T3'/>
+            </div>
         </div> 
+
         </>
     )
 }
