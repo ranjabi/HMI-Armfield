79618b91a0d9b0e756ea5a6a373e307957e5ba58
Muhammad Hanif Al Faithoni <et4w4zz@gmail.com>
2023-06-01 23:47:16 +0700

    input S-P

--- src/pages/HMI/index.jsx
+++ src/pages/HMI/index.jsx
@@ -1,6 +1,6 @@
 import Header from '../../component/header'
 import './style.css'
-import IM from '../../assets/Gambar Ilustrasi Mesin (No BG).png';
+import IM from '../../assets/HMI.svg';
 
 const HMI = () => {
     return (
@@ -9,12 +9,14 @@ const HMI = () => {
         <div className='ilustrasi'>
             <img src= {IM} alt="Gambar Ilustrasi Mesin" />
             <div className='T1'>
-                <label htmlFor="a">T1</label>
-                <input type="number" id='a' name='a' placeholder='T1'/>
+                <span className='spanT1'>S -P</span>
+                <input type="number" id='a' name='a' placeholder=''/>
+                <span> °C</span>
             </div>
             <div className='T3'>
-                <label htmlFor="b">T3</label>
-                <input type="number" id='b' name='b' placeholder='T3'/>
+                <span className='spanT3'>S -P</span>
+                <input type="number" id='b' name='b' placeholder=''/>
+                <span> °C</span>
             </div>
         </div> 
 
