9f1a30c22cce54152044e324d4cde08f8ce7a206
ardhanurfan <ardhanurfan@gmail.com>
2023-06-07 11:19:59 +0700

    dashboard

--- src/component/linechart/index.jsx
+++ src/component/linechart/index.jsx
@@ -1,17 +1,22 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 // import {Chart} from 'chart.js';
-import { Chart, registerables } from 'chart.js';
-import "./style.css"
-import { ChartConfiguration, LineController, LineElement, PointElement, LinearScale, Title} from 'chart.js'
+import { Chart, registerables } from "chart.js";
+import "./style.css";
+import {
+  LineController,
+  LineElement,
+  PointElement,
+  LinearScale,
+  Title,
+} from "chart.js";
 
 Chart.register(...registerables);
 Chart.register(LineController, LineElement, PointElement, LinearScale, Title);
 const LineChart = ({ data }) => {
-
   const chartRef = useRef(null);
 
   useEffect(() => {
-    const ctx = chartRef.current.getContext('2d');
+    const ctx = chartRef.current.getContext("2d");
 
     const chart = new Chart(ctx, {
       type: "line",
@@ -20,10 +25,10 @@ const LineChart = ({ data }) => {
         plugins: {
           legend: {
             display: true,
-            position: 'bottom',
+            position: "bottom",
             labels: {
-              usePointStyle: true, 
-              pointStyle: 'circle', 
+              usePointStyle: true,
+              pointStyle: "circle",
             },
           },
         },
@@ -35,7 +40,7 @@ const LineChart = ({ data }) => {
     };
   }, [data]);
 
-  return <canvas ref={chartRef}/>;
+  return <canvas ref={chartRef} />;
 };
 
 export default LineChart;
--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,105 +1,169 @@
-import Header from '../../component/header'
-import './style.css'
-import Arrows from '../../assets/ArrowsOut.svg'
-import Minus from '../../assets/MinusCircle.svg'
-import Hand from '../../assets/Hand.svg'
-import Plus from '../../assets/PlusCircle.svg'
-import Graph from '../../assets/Chart.svg'
-import LineChart from '../../component/linechart'
+import Header from "../../component/header";
+import "./style.css";
+import Arrows from "../../assets/ArrowsOut.svg";
+import Minus from "../../assets/MinusCircle.svg";
+import Hand from "../../assets/Hand.svg";
+import Plus from "../../assets/PlusCircle.svg";
+import Graph from "../../assets/Chart.svg";
+import LineChart from "../../component/linechart";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Dashboard = () => {
-    const chartData = {
-        labels: ['0', '1', '2', '3', '4', '5'],
-        datasets: [
-          {
-            label: 'Data Set 1',
-            data: [8, 8, 3, 5, 2, 3],
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgba(255, 99, 132, 1)',
-            borderWidth: 1,
-          },
-          {
-            label: 'Data Set 2',
-            data: [7, 2, 5, 8, 3, 7],
-            backgroundColor: 'rgba(54, 162, 235, 0.2)',
-            borderColor: 'rgba(54, 162, 235, 1)',
-            borderWidth: 1,
-          },
-        ],
-      };
-    return (
-        <>
-            <Header />
-            <div className="dashbox">
-                <div className="grid-container">
-                    <div className="item">
-                        <h5>Level Tank A (mm)</h5>
-                        <p>190</p>
-                    </div>
-                    <div className="item">
-                        <h5>Pump N1 (%)</h5>
-                        <p>60</p>
-                    </div>
-                    <div className="item">
-                        <h5>Pump N2 (%)</h5>
-                        <p>80</p>
-                    </div>
-                    <div className="item">
-                        <h5>Heater Power (Kwh)</h5>
-                        <p>0.37</p>
-                    </div>
-                    <div className='item item5'>
-                        <div className="graphttl">
-                            <h6>Holding Temperatur T1</h6>
-                            <div className="imgbox">
-                                <img src={Arrows}/>
-                                <img src={Plus}/>
-                                <img src={Minus}/>
-                                <img src={Hand}/>
-                            </div>
-                        </div>   
-                        <LineChart data={chartData}/> 
-                    </div>
-                    <div className="item item6">
-                        <div className="graphttl">
-                            <h6>Heater Temperatur T2</h6>
-                            <div className="imgbox">
-                                <img src={Arrows}/>
-                                <img src={Plus}/>
-                                <img src={Minus}/>
-                                <img src={Hand}/>
-                            </div>
-                        </div>   
-                        <LineChart data={chartData}/>
-                    </div>
-                    <div className="item item7">
-                        <div className="graphttl">
-                            <h6>Heated Feed Temperatur T4</h6>
-                            <div className="imgbox">
-                                <img src={Arrows}/>
-                                <img src={Plus}/>
-                                <img src={Minus}/>
-                                <img src={Hand}/>
-                            </div>
-                        </div>   
-                        <LineChart data={chartData}/>
-                    </div>
-                    <div className="item item8">
-                        <div className="graphttl">
-                            <h6>Feed Flow F1</h6>
-                            <div className="imgbox">
-                                <img src={Arrows}/>
-                                <img src={Plus}/>
-                                <img src={Minus}/>
-                                <img src={Hand}/>
-                            </div>
-                        </div>   
-                        <LineChart data={chartData}/> 
-                    </div>
-                </div>
+  const [meanTankA, setMeanTankA] = useState(0);
+  const [meanPump1, setMeanPump1] = useState(0);
+  const [meanPump2, setMeanPump2] = useState(0);
+  const [meanHeater, setMeanHeater] = useState(0);
+  const [timestamp, setTimestamp] = useState([]);
+  const [setPointT1, setSetPointT1] = useState([]);
+  const [holdingT1, setHoldingT1] = useState([]);
+
+  const getData = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost/armfield/public/api/data-plant-all"
+      );
+      let data = response.data?.data;
+      let sumTankA = 0;
+      let sumPump1 = 0;
+      let sumPump2 = 0;
+      let sumHeater = 0;
+
+      let tempTime = [];
+      let tempSetPointT1 = [];
+      let tempHoldingT1 = [];
+      data.forEach((e) => {
+        tempSetPointT1.push(e.set_point_t1);
+        tempHoldingT1.push(e.holding_t1);
+
+        sumTankA = sumTankA + e.tank_a;
+        sumPump1 = sumPump1 + e.pump_n1;
+        sumPump2 = sumPump2 + e.pump_n2;
+        sumHeater = sumHeater + e.power_heater;
+
+        var dateFormat = new Date(e.created_at);
+        var date =
+          dateFormat.getDate() +
+          "/" +
+          (dateFormat.getMonth() + 1) +
+          "/" +
+          dateFormat.getFullYear() +
+          " " +
+          dateFormat.getHours() +
+          ":" +
+          dateFormat.getMinutes() +
+          ":" +
+          dateFormat.getSeconds();
+        tempTime.push(date);
+      });
+      setMeanTankA(sumTankA / data.length);
+      setMeanPump1(sumPump1 / data.length);
+      setMeanPump2(sumPump2 / data.length);
+      setMeanHeater(sumHeater / data.length);
+      setTimestamp(tempTime);
+      setSetPointT1(tempSetPointT1);
+      setHoldingT1(tempHoldingT1);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const chartDataTemp1 = {
+    labels: timestamp,
+    datasets: [
+      {
+        label: "Set Point",
+        data: setPointT1,
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+        borderColor: "rgba(255, 99, 132, 1)",
+        borderWidth: 1,
+      },
+      {
+        label: "T1",
+        data: holdingT1,
+        backgroundColor: "rgba(54, 162, 235, 0.2)",
+        borderColor: "rgba(54, 162, 235, 1)",
+        borderWidth: 1,
+      },
+    ],
+  };
+  return (
+    <>
+      <Header />
+      <div className="dashbox">
+        <div className="grid-container">
+          <div className="item">
+            <h5>Level Tank A (mm)</h5>
+            <p>{meanTankA}</p>
+          </div>
+          <div className="item">
+            <h5>Pump N1 (%)</h5>
+            <p>{meanPump1}</p>
+          </div>
+          <div className="item">
+            <h5>Pump N2 (%)</h5>
+            <p>{meanPump2}</p>
+          </div>
+          <div className="item">
+            <h5>Heater Power (Kwh)</h5>
+            <p>{meanHeater}</p>
+          </div>
+          <div className="item item5">
+            <div className="graphttl">
+              <h6>Holding Temperatur T1</h6>
+              <div className="imgbox">
+                <img src={Arrows} />
+                <img src={Plus} />
+                <img src={Minus} />
+                <img src={Hand} />
+              </div>
             </div>
-        </>
-    )
-}
+            <LineChart data={chartDataTemp1} />
+          </div>
+          <div className="item item6">
+            <div className="graphttl">
+              <h6>Heater Temperatur T2</h6>
+              <div className="imgbox">
+                <img src={Arrows} />
+                <img src={Plus} />
+                <img src={Minus} />
+                <img src={Hand} />
+              </div>
+            </div>
+            <LineChart data={chartDataTemp1} />
+          </div>
+          <div className="item item7">
+            <div className="graphttl">
+              <h6>Heated Feed Temperatur T4</h6>
+              <div className="imgbox">
+                <img src={Arrows} />
+                <img src={Plus} />
+                <img src={Minus} />
+                <img src={Hand} />
+              </div>
+            </div>
+            <LineChart data={chartDataTemp1} />
+          </div>
+          <div className="item item8">
+            <div className="graphttl">
+              <h6>Feed Flow F1</h6>
+              <div className="imgbox">
+                <img src={Arrows} />
+                <img src={Plus} />
+                <img src={Minus} />
+                <img src={Hand} />
+              </div>
+            </div>
+            <LineChart data={chartDataTemp1} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Dashboard;
